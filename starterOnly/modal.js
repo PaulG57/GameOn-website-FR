9c576e8d9cd139a377158e4bb67284794422c2f0
Paul Glatt <paul.glatt57@gmail.com>
2024-06-01 04:48:06 +0200

    mise en place messages erreurs

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -17,7 +17,7 @@ let champPrenom = document.getElementById("first");
 let champNom = document.getElementById("last");
 let champEmail = document.getElementById("email");
 let champQuantity = document.getElementById("quantity");
-let divLocation = document.getElementById("locations");
+let divAfficherErreur = document.querySelector(".formData");
 let radioLocation = document.querySelectorAll("input[name=location]");
 let checkboxConditions = document.getElementById("checkbox1");
 let divConditions = document.getElementById("conditions");
@@ -40,63 +40,83 @@ function closeModal() {
 
 // validate form
 function validerChamp(champ) {
+  let classList = ["error", "elementMessageErreur"];
   if (champ.value.length < 2) {
     champ.classList.add("error");
-    return false;
+    divAfficherErreur.classList.add("elementMessageErreur");
+    throw new Error(`Le ${champ.name} n'est pas valide.(2 caractères minimum)`);
   }
   champ.classList.remove("error");
-  return true;
+  divAfficherErreur.classList.remove("elementMessageErreur");
 }
 
 function validerEmail(email) {
   let emailRegExp = new RegExp("[a-z0-9._-]+@[a-z0-9._-]+\\.[a-z0-9._-]+");
-  if (emailRegExp.test(email.value)) {
-    email.classList.remove("error");
-    return true;
+  if (!emailRegExp.test(email.value)) {
+    email.classList.add("error");
+    throw new Error("L'email n'est pas valide.");
   }
-  email.classList.add("error");
-  return false;
+  email.classList.remove("error");
 }
 
 function validerNombre (quantity) {
   let regex = new RegExp("^[0-9]+$");
-  if (regex.test(quantity.value)) {
-    quantity.classList.remove("error");
-    return true;
+  if (!regex.test(quantity.value)) {
+    quantity.classList.add("error");
+    throw new Error("La quantité n'est pas valide.");
   }
-  quantity.classList.add("error");
-  return false;
+  quantity.classList.remove("error");
 }
 
 function validerLocation(location) {
+  let selected = false;
   for (let i = 0; i < location.length; i++) {
     if (location[i].checked) {
-      divLocation.classList.remove("error");
-      return true;
+      selected = true;
+      break;
     }
+    selected = false;
   }
-  divLocation.classList.add("error");
-  return false;
+
+  if (!selected) {
+    divAfficherErreur.classList.add("error");
+    throw new Error("La location n'est pas valide.");
+  }
+  divAfficherErreur.classList.remove("error");
 }
 
 function validerConditions(condition) {
   if (!condition.checked) {
     divConditions.classList.add("error");
-    return false;
+    throw new Error("La condition n'est pas valide.");
   }
   divConditions.classList.remove("error");
-  return true;
+}
+
+function afficherMessageErreur(message) {
+  let divMessageErreur = document.querySelector(".elementMessageErreur");
+  let spanErreurMessage = document.getElementById("erreurMessage");
+  if (!spanErreurMessage) {
+  spanErreurMessage = document.createElement("span");
+  spanErreurMessage.id = "erreurMessage";
+  spanErreurMessage.innerText = message;
+  divMessageErreur.appendChild(spanErreurMessage);
+}
 }
 
 // submit form
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  validerChamp(champPrenom);
-  validerChamp(champNom);
-  validerEmail(email);
-  validerNombre(champQuantity);
-  validerLocation(radioLocation);
-  validerConditions(checkboxConditions);
+  try {
+    validerChamp(champPrenom);
+    validerChamp(champNom);
+    validerEmail(email);
+    validerNombre(champQuantity);
+    validerLocation(radioLocation);
+    validerConditions(checkboxConditions);
+  } catch (erreur) {
+    afficherMessageErreur(erreur.message);
+    }
 
 })
