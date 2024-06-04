35850ca9a5a5817b0340af4c55074256f5f0937e
Paul Glatt <paul.glatt57@gmail.com>
2024-06-04 17:59:24 +0200

    messages erreur

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -17,10 +17,12 @@ let champPrenom = document.getElementById("first");
 let champNom = document.getElementById("last");
 let champEmail = document.getElementById("email");
 let champQuantity = document.getElementById("quantity");
-let divAfficherErreur = document.querySelector(".formData");
+//let divAfficherErreur = document.querySelector(".formData");
 let radioLocation = document.querySelectorAll("input[name=location]");
+let divLocation = document.getElementById("locations");
 let checkboxConditions = document.getElementById("checkbox1");
 let divConditions = document.getElementById("conditions");
+let classList = ["error", "elementMessageErreur"];
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -40,36 +42,43 @@ function closeModal() {
 
 // validate form
 function validerChamp(champ) {
-  let classList = ["error", "elementMessageErreur"];
   if (champ.value.length < 2) {
     champ.classList.add("error");
-    divAfficherErreur.classList.add("elementMessageErreur");
+    champ.parentNode.classList.add("elementMessageErreur");
     throw new Error(`Le ${champ.name} n'est pas valide.(2 caractères minimum)`);
   }
   champ.classList.remove("error");
-  divAfficherErreur.classList.remove("elementMessageErreur");
+  champ.parentNode.classList.remove("elementMessageErreur");
+  return true;
 }
 
 function validerEmail(email) {
   let emailRegExp = new RegExp("[a-z0-9._-]+@[a-z0-9._-]+\\.[a-z0-9._-]+");
   if (!emailRegExp.test(email.value)) {
     email.classList.add("error");
+    email.parentNode.classList.add("elementMessageErreur");
     throw new Error("L'email n'est pas valide.");
   }
   email.classList.remove("error");
+  email.parentNode.classList.remove("elementMessageErreur");
+  return true;
 }
 
 function validerNombre (quantity) {
   let regex = new RegExp("^[0-9]+$");
   if (!regex.test(quantity.value)) {
     quantity.classList.add("error");
+    quantity.parentNode.classList.add("elementMessageErreur");
     throw new Error("La quantité n'est pas valide.");
   }
   quantity.classList.remove("error");
+  quantity.parentNode.classList.remove("elementMessageErreur");
+  return true;
 }
 
 function validerLocation(location) {
   let selected = false;
+
   for (let i = 0; i < location.length; i++) {
     if (location[i].checked) {
       selected = true;
@@ -79,29 +88,31 @@ function validerLocation(location) {
   }
 
   if (!selected) {
-    divAfficherErreur.classList.add("error");
+    divLocation.classList.add(...classList);
     throw new Error("La location n'est pas valide.");
   }
-  divAfficherErreur.classList.remove("error");
+  divLocation.classList.remove(...classList);
+  return true;
 }
 
 function validerConditions(condition) {
   if (!condition.checked) {
-    divConditions.classList.add("error");
+    divConditions.classList.add(...classList);
     throw new Error("La condition n'est pas valide.");
   }
-  divConditions.classList.remove("error");
+  divConditions.classList.remove(...classList);
+  return true;
 }
 
 function afficherMessageErreur(message) {
-  let divMessageErreur = document.querySelector(".elementMessageErreur");
   let spanErreurMessage = document.getElementById("erreurMessage");
   if (!spanErreurMessage) {
-  spanErreurMessage = document.createElement("span");
-  spanErreurMessage.id = "erreurMessage";
-  spanErreurMessage.innerText = message;
-  divMessageErreur.appendChild(spanErreurMessage);
-}
+    let divMessageErreur = document.querySelector(".elementMessageErreur");
+    spanErreurMessage = document.createElement("span");
+    spanErreurMessage.id = "erreurMessage";
+    divMessageErreur.append(spanErreurMessage);
+  }
+    spanErreurMessage.innerText = message;
 }
 
 // submit form
@@ -115,6 +126,7 @@ form.addEventListener("submit", (event) => {
     validerNombre(champQuantity);
     validerLocation(radioLocation);
     validerConditions(checkboxConditions);
+    afficherMessageErreur("");
   } catch (erreur) {
     afficherMessageErreur(erreur.message);
     }
